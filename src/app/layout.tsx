d8b7df9b5b15f3a675da2704643296d4d7413c52
jakub-rudnik <jakub.rudnik@interia.pl>
2025-03-03 21:39:21 +0100

    dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,19 +2,33 @@ import "@/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
   title: "Flatoinator3000",
-  description: "We bully our good friend hehe :) She allowed it so don't worry :)",
-  icons: [{ rel: "icon", url: "/favicon.ico" }]
+  description:
+    "We bully our good friend hehe :) She allowed it so don't worry :)",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+    <html lang="pl" className={GeistSans.variable} suppressHydrationWarning>
+      <head />
+      <body className="min-h-screen bg-background font-sans antialiased">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="theme-preference" // Add a storage key
+          enableColorScheme={true} // Explicitly enable color scheme
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
