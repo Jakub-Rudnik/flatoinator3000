033d7a27b83c921e02ebc017f1cb153c86e3148e
jakub-rudnik <jakub.rudnik@interia.pl>
2025-03-09 23:11:18 +0100

    feat: add clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,14 @@ import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "@/components/theme-toggle";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Flatoinator3000",
@@ -16,34 +24,44 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="pl" className={GeistSans.variable} suppressHydrationWarning>
-      <head />
-      <body className="flex min-h-screen flex-col items-center justify-between bg-background font-sans antialiased">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-          storageKey="theme-preference" // Add a storage key
-          enableColorScheme={true} // Explicitly enable color scheme
-        >
-          {children}
-          <footer className="flex w-full items-center justify-center gap-5 border-t-2 p-4 text-center">
-            <span>
-              {" "}
-              Built by Jakub Rudnik. Source code on{" "}
-              <a
-                className="font-medium text-primary underline underline-offset-4"
-                href="https://github.com/Jakub-Rudnik/flatoinator3000"
-                target="_blank"
-              >
-                github
-              </a>
-            </span>
-            <ModeToggle />
-          </footer>
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="pl" className={GeistSans.variable} suppressHydrationWarning>
+        <body className="flex min-h-screen flex-col items-center justify-between bg-background font-sans antialiased">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="theme-preference"
+            enableColorScheme={true}
+          >
+            <header className="flex items-center justify-end gap-4 p-4">
+              <SignedOut>
+                <SignInButton />
+                <SignUpButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </header>
+            {children}
+            <footer className="flex w-full items-center justify-center gap-5 border-t-2 p-4 text-center">
+              <span>
+                {" "}
+                Built by Jakub Rudnik. Source code on{" "}
+                <a
+                  className="font-medium text-primary underline underline-offset-4"
+                  href="https://github.com/Jakub-Rudnik/flatoinator3000"
+                  target="_blank"
+                >
+                  github
+                </a>
+              </span>
+              <ModeToggle />
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -3,10 +3,12 @@
 import { Button } from "@/components/ui/button";
 import { useAppState } from "@/lib/state-context";
 import { useState } from "react";
+import { useUser } from "@clerk/nextjs";
 
 export default function Counter() {
   const { amount, incrementCounter } = useAppState();
   const [isIncrementing, setIsIncrementing] = useState(false);
+  const { isSignedIn } = useUser();
 
   const handleClick = async () => {
     if (isIncrementing) return;
@@ -24,14 +26,18 @@ export default function Counter() {
       <h2 className="scroll-m-20 pb-2 text-5xl font-semibold tracking-tight first:mt-0">
         {amount}
       </h2>
-      <Button
-        size="lg"
-        onClick={handleClick}
-        disabled={isIncrementing}
-        className="bg-[url(/wood.webp)] bg-cover bg-center text-white"
-      >
-        Wypłaszcz
-      </Button>
+      {isSignedIn ? (
+        <Button
+          size="lg"
+          onClick={handleClick}
+          disabled={isIncrementing}
+          className="bg-[url(/wood.webp)] bg-cover bg-center text-white"
+        >
+          Wypłaszcz
+        </Button>
+      ) : (
+        "Zaloguj się, by wypłaszczyć"
+      )}
     </>
   );
 }
